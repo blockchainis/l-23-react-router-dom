b9fe74fe27576aa91c016003da87b94f8eb57e3f
hoon2 <sanghoon.engineer@gmail.com>
2022-10-19 07:10:53 +0900

    리엑트 라우터 돔 초기코드

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Jenny from "./components/Jenny";
 import Jisoo from "./components/Jisoo";
 import Lisa from "./components/Lisa";
 import Rose from "./components/Rose";
-import Layout from "./components/Layout";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />}></Route>
-        <Route path="jenny" element={<Jenny />}></Route>
-        <Route path="jisoo" element={<Jisoo />}></Route>
-        <Route path="lisa" element={<Lisa />}></Route>
-        <Route path="rose" element={<Rose />}></Route>
-      </Route>
-    </Routes>
-  );
+  return <Home />;
 }
 
 export default App;
